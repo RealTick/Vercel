8963f2f88cb891db370c914c2794f5bfafc49d74
SandroAma <50810024+sandroama@users.noreply.github.com>
2023-11-29 18:41:39 -0500

    fixed URLs

--- RealTick/src/components/StockRealTimeService.jsx
+++ RealTick/src/components/StockRealTimeService.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const fetchRealTimeData = async (symbol) => {
   try {
     const response = await axios.get(
-      `http://127.0.0.1:5000/realtime_stock?symbol=${symbol}`
+      `https://realtick.pythonanywhere.com/realtime_stock?symbol=${symbol}`
     );
     return response.data;
   } catch (err) {
